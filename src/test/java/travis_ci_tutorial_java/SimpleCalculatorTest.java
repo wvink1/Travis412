b8e200e0db5a0b88b25f2f7fc59d24a63b64b066
wvink1 <124178145+wvink1@users.noreply.github.com>
2023-03-06 21:45:15 -0500

    Update SimpleCalculatorTest.java
    Submitting the tests for multiply and divide

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -17,4 +17,16 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.minus(3, 2), 1);
 	}
+	
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(4, 2), 2);
+	}
+	
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(3, 3), 9);
+	}
 }
